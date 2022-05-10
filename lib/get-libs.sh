d0c36fc84c8ed7dfcdc36cd6fbfcaee3e2685296
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2022-05-10 16:24:42 +0200

    comments added

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,3 +1,11 @@
+# File:         get-libs.sh
+# Institution:  FIT BUT 2021/2022
+# Course:       IJA - Java Programming Language
+# Authors:      Lucie Svobodová, xsvobo1x@stud.fit.vutbr.cz
+#               Jakub Kuzník, xkuzni04@stud.fit.vutbr.cz
+#
+# Script downloads necessary libraries.
+ 
 # Download JavaFX
 wget https://download2.gluonhq.com/openjfx/18/openjfx-18_linux-x64_bin-sdk.zip
 unzip openjfx-18_linux-x64_bin-sdk.zip
@@ -8,6 +16,3 @@ unzip json-simple-1.1.1.jar.zip
 mv javafx-sdk-18/lib/* .
 rm -r javafx-sdk-18/
 
-# Download Apache IOUtils
-#wget http://www.java2s.com/Code/JarDownload/org.apache.commons/org.apache.commons.io.jar.zip
-#unzip org.apache.commons.io.jar.zip
